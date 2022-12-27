d3736a33ea26eaebfaa73faca6306c1578799e32
elhabib ibaline <h.ibaline@gmail.com>
2022-12-28 00:16:13 +0100

    create-product

--- index.js
+++ index.js
@@ -26,3 +26,34 @@ function getTotal() {
       total.style.background = "#a00d02"; //rouge
     }
   }
+
+
+/*=========================
+CREATE: 1ere operation crud
+============================== */
+
+let dataPro;
+
+if (localStorage.product != null) {
+  dataPro = JSON.parse(localStorage.product);
+} else {
+  dataPro = [];
+}
+
+submit.onclick = function () {
+  let newPro = {
+    title: title.value,
+    price: price.value,
+    taxes: taxes.value,
+    ads: ads.value,
+    discount: discount.value,
+    total: total.innerHTML,
+    count: count.value,
+    category: category.value,
+  };
+
+  dataPro.push(newPro);
+
+  localStorage.setItem("product", JSON.stringify(dataPro));
+
+};
